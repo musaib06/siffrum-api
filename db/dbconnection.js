32f49fdf189f66d902b272fcc5b9be34ad118c3b
Musaib Gul <dev.renosoftwares@gmail.com>
2025-07-10 17:28:36 +0530

    updated connection string with supabase and koyeb

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
 import createUserModel from "../model/userModel.js";
 import createLicenseModel from "../model/licenses.model.js";
 import createModulesModel from "../model/modules.model.js";
@@ -6,32 +9,50 @@ import createBannerModel from "../model/banner.model.js";
 import createLicenseModuleModel from "../model/license_module.model.js";
 import createUserInvoiceModel from "../model/user.invoices.model.js";
 import createUserLicenseDetailsModel from "../model/user.license.details.model.js";
+
 let User = null;
 let License = null;
 let Module = null;
 let Banner = null;
-let LicenseModule=null;
+let LicenseModule = null;
 let UserInvoice = null;
-let UserLicenseDetails= null;
+let UserLicenseDetails = null;
+
 export const dbConnection = async (database, username, password) => {
   const sequelize = new Sequelize(database, username, password, {
-    host: "localhost",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   });
+
   try {
     await sequelize.authenticate();
     User = await createUserModel(sequelize);
-    License= await createLicenseModel(sequelize);
-    Module= await createModulesModel(sequelize);
+    License = await createLicenseModel(sequelize);
+    Module = await createModulesModel(sequelize);
     Banner = await createBannerModel(sequelize);
     LicenseModule = await createLicenseModuleModel(sequelize);
     UserInvoice = await createUserInvoiceModel(sequelize);
     UserLicenseDetails = await createUserLicenseDetailsModel(sequelize);
-    await sequelize.sync({alter:true});
-    console.log('Connection has been established successfully.');
+
+    await sequelize.sync({ alter: true });
+
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 };
 
-export {User,License,Module,Banner};
+export { User, License, Module, Banner };
+
+// export const dbConnection = async (database, username, password) => {
+//   const sequelize = new Sequelize(database, username, password, {
+//     host: "localhost",
+//     dialect: "postgres",
+//   });
